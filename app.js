511050799506f190a6afbc73a38048a909ea5e15
Vedaant-Rajoo <vedaant12345@gmail.com>
2019-03-03 02:27:17 +0530

    added passport

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var express=require("express");
+var mongoose=require("mongoose"),
+    passport=require("passport"),
+    User=require("./models/user"),
+    LocalStrategy=require("passport-local"),
+    passportLocalMongoose=require("passport-local-mongoose")
+
 var app=express();
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+app.use(require("express-session")({
+    secret:"Rusty is the cutest",
+    resave:false,
+    saveUninitialized: false
+
+}));
 app.set('view engine','ejs');
+var bodyParser=require("body-parser");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/secret",function(){
