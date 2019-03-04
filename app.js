7fc97b839e076f8d998abca9786a26de203658b3
Vedaant-rajoo <vedaant12345@gmail.com>
2019-03-04 23:04:49 +0530

    app:

--- app.js
+++ app.js
@@ -89,6 +89,7 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
 
 }
-app.listen("3000",function(){
-    console.log("Serving passport on port 3000");
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log("Serving passport on port",port);
 });
